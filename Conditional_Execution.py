bc1786f34f0b0529c1d9a6a12b820328060dddc1
CQuinnce <canadaquinton1@gmail.com>
2024-12-20 05:34:31 -0600

    Staring with e.2 Add e.1 once complete with e.2 exercises will now be shortend to e.num

--- Conditional_Execution.py
+++ Conditional_Execution.py
@@ -1,2 +1,21 @@
 Chapter_3 = "Conditional Execution"
-print("Testing!")
+
+def exercise_1():
+    hours = None
+    rate = None
+    
+    try:
+        working_hours = input("How many hours do you work: ")
+        hours = float(working_hours)
+        print(f"You work: {working_hours} hours")
+        hourly_rate = input("What is your hourly rate: ")
+        rate = float(hourly_rate)
+        print(f"Your current hourly rate is: ${rate}")
+        pay = hours * rate
+        print(f"Your gross paycheck for this pay period is: ${pay}")
+        print(f"Should your job round to the nearest dollar your pay is now: ${round(pay)}")
+    
+    except:
+        print("Incorrect value entered.")
+        return
+exercise_1()
