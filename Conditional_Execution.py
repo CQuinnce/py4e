7eb16d19bfb72519c7b24a1a9851e41beb03ce63
CQuinnce <canadaquinton1@gmail.com>
2024-12-20 06:15:08 -0600

    First trial exercise praticing the comparison operators.

--- Conditional_Execution.py
+++ Conditional_Execution.py
@@ -1,1 +1,10 @@
 Chapter_3 = "Conditional Execution"
+
+def tesing_comparison_operators():
+    x = 3
+    if x < 10:
+        print("small")
+
+    print("fucntion done")
+
+#tesing_comparison_operators()
